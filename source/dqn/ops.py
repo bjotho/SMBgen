550a267b4c3000400a9684c747b89ab8d4d7e64c
Rubicoy <31884006+Rubicoy@users.noreply.github.com>
2020-02-20 11:52:18 +0100

    Fixed tensorflow implementation. May have hit a PyCharm bug

--- source/dqn/ops.py
+++ source/dqn/ops.py
@@ -1,26 +1,27 @@
 import tensorflow as tf
 import numpy as np
-from keras import regularizers, 
+from tensorflow import keras
 '''l2_regularizer, xavier_initializer, fully_connected, flatten'''
 
 
 # various implementations of this exist, particularly in how the negatives are calculated
 # for more, read https://arxiv.org/abs/1502.01852
 def parametric_relu(X, regularizer=None, name="parametric_relu"):
-    with tf.variable_scope(name):
-        alphas = tf.get_variable('alphas',
+    #with tf.compat.v1.variable_scope(name):
+    with tf.Variable(name):
+        alphas = tf.Variable('alphas',
                                  regularizer=regularizer,
                                  dtype=X.dtype,
                                  shape=X.get_shape().as_list()[-1],
-                                 initializer=tf.constant_initializer(0.01))
+                                 initializer=tf.keras.regularizers.l2(0.01))
         positives = tf.nn.relu(X)
-        negatives = alphas * (tf.subtract(X, tf.abs(X))) * 0.5
+        negatives = alphas * (tf.math.subtract(X, tf.math.abs(X))) * 0.5
         return positives + negatives
 
 
 # this implementation assumes alpha will be less than 1. It would be stupid if it weren't.
 def leaky_relu(X, alpha=0.2):
-    return tf.maximum(X, alpha * X)
+    return tf.math.maximum(X, alpha * X)
 
 
 # https://arxiv.org/abs/1302.4389
@@ -33,8 +34,8 @@ def maxout(X, num_maxout_units, axis=None):
     num_filters = input_shape[axis]
 
     if num_filters % num_maxout_units != 0:
-        raise ValueError, "num filters (%d) must be divisible by num maxout units (%d)" % (
-        num_filters, num_maxout_units)
+        raise [ValueError, "num filters (%d) must be divisible by num maxout units (%d)" % (
+        num_filters, num_maxout_units)]
 
     output_shape = input_shape.copy()
     output_shape[axis] = num_maxout_units
@@ -46,16 +47,16 @@ def conv(X,
          output_filter_size,
          kernel=[5, 5],
          strides=[2, 2],
-         w_initializer=xavier_initializer(),
+         w_initializer=tf.keras.initializers.xavier_initializer(),
          regularizer=None,
          name="conv"):
-    with tf.variable_scope(name):
-        W = tf.get_variable('W_conv',
+    with tf.Variable(name):
+        W = tf.Variable('W_conv',
                             regularizer=regularizer,
                             dtype=X.dtype,
                             shape=[kernel[0], kernel[1], X.get_shape().as_list()[-1], output_filter_size],
                             initializer=w_initializer)
-        b = tf.get_variable('b_conv',
+        b = tf.Variable('b_conv',
                             regularizer=regularizer,
                             dtype=X.dtype,
                             shape=[output_filter_size],
